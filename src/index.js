94b456196a6817e0292f02812a59fa4a89a741fc
akshaypandey28 <akshaychaman2004@gmail.com>
2026-05-09 00:36:19 +0530

    added Message Queue for publishing and subscribing message from the queue

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ const express = require('express');
 
 const { PORT } = require('./config/serverConfig.js');
 
+const EmailService = require('./services/email-service.js')
+const {createChannel,subscribeMessage} = require('./utils/messageQueue.js')
 const jobs = require('./utils/job.js');
+const {REMINDER_BINDING_KEY} = require('./config/serverConfig.js');
 
 const ApiRoutes = require('./routes/index.js'); //ApiRoutes is a router
 
@@ -14,10 +17,15 @@ const setupAndStartServer = async () => {
     app.use(express.urlencoded({extended: true}));
 
     app.use('/api',ApiRoutes); 
+
+    const channel = await createChannel();
+
+    subscribeMessage(channel,EmailService.subscribeEvents,REMINDER_BINDING_KEY);
+
     
     app.listen(PORT, () => {
         console.log(`Server started at port ${PORT}`);
-        jobs();
+        //jobs();
     });
 }
 
